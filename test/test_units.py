7ea2d00ddb575dc865754d7e8d601b044f2950eb
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-08-15 17:45:37 -0500

    add test for pptv

--- test/test_units.py
+++ test/test_units.py
@@ -9,3 +9,6 @@ def test_ppmv():
 def test_ppbv():
     abn = 1*units.ppbv
     assert abn.to_value(u.dimensionless_unscaled) == pytest.approx(1e-9,abs=1e-12)
+def test_pptv():
+    abn = 1*units.pptv
+    assert abn.to_value(u.dimensionless_unscaled) == pytest.approx(1e-12,abs=1e-12)
