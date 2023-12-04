2e2a97db9577e1da7cd104dd18e52a59166c7e41
Ted Johnson <ted.johnson@unlv.edu>
2023-12-04 12:31:14 -0800

    add PyRad to top level

--- pypsg/__init__.py
+++ pypsg/__init__.py
@@ -6,5 +6,6 @@ PyPSG top-level module
 from pypsg.request import APICall
 from pypsg import cfg
 from pypsg.cfg import PyConfig
+from pypsg.rad import PyRad
 from pypsg import settings
 from pypsg import units
