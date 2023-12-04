c790dbacae8dd08f968f838f02a06ec32ba5efe8
Ted Johnson <ted.johnson@unlv.edu>
2023-12-04 12:45:50 -0800

    add PyLyr to top level

--- pypsg/__init__.py
+++ pypsg/__init__.py
@@ -7,5 +7,6 @@ from pypsg.request import APICall
 from pypsg import cfg
 from pypsg.cfg import PyConfig
 from pypsg.rad import PyRad
+from pypsg.lyr import PyLyr
 from pypsg import settings
 from pypsg import units
