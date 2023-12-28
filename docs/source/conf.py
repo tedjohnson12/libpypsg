16d00093ab4fac886185fb504122ce6bfd612ef3
Ted Johnson <ted.johnson@unlv.edu>
2023-12-28 13:21:51 -0800

    version 0.1.0 --> 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'pypsg'
 copyright = '2023, Ted Johnson, The PSG Team'
 author = 'Ted Johnson, The PSG Team'
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
