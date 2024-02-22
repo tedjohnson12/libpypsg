fix units when computing altitude

pypsg/globes/globes.py
@@ -439,7 +439,7 @@ class PyGCM:
             pressure_top = pressure.dat[i+1, :, :].to(u.bar)
             dP = pressure_top - pressure_bottom
             # pylint: disable-next=no-member
-            rho = mean_molecular_mass * \
+            rho = mean_molecular_mass*u.Unit('g mol-1') * \
                 (pressure_bottom + 0.5*dP) / c.R / temperature.dat[i, :, :]
             distance_from_planet_center = radius + z[i, :, :]*z_unit
             # pylint: disable-next=no-member
@@ -511,7 +511,7 @@ class PyGCM:
         u.Quantity
             The column density of the aerosol.
         """
-        mass_frac = aerosol.dat.to_value(u.dimensionless_unscaled)
+        mass_frac = aerosol.dat.to_value(u.dimensionless_unscaled)[:-1,:,:]
         heights = np.diff(altitude, axis=0)
         mean_molecular_mass = mean_molecular_mass * u.g/u.mol
         # pylint: disable-next=no-member