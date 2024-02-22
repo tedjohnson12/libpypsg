8a6243adbef1241bab6aa10ba7ccc4123fb275c4
Ted Johnson <ted.johnson@unlv.edu>
2024-02-22 06:50:51 -0800

    increment version

--- pypsg/__init__.py
+++ pypsg/__init__.py
@@ -2,7 +2,7 @@
 PyPSG top-level module
 ======================
 """
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 from .request import APICall, PSGResponse
 from . import cfg
 from .cfg import PyConfig
