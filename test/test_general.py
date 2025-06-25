b613ab5bdf3ca735213e2b0b76c96e986b35cc9a
Ted Johnson <ted.johnson@unlv.edu>
2025-06-25 10:36:05 -0700

    Check for release branch name

--- test/test_general.py
+++ test/test_general.py
@@ -1,19 +1,26 @@
 """
 Some general quality-of-life tests
 """
-
+import sys
 from pathlib import Path
-import pytest
-
+from subprocess import Popen, PIPE
 import tomllib
+import pytest
 from libpypsg import __version__
 
-import sys
+
 
 DOCS_SOURCE_PATH = Path(__file__).parent.parent / 'docs' / 'source'
 sys.path.append(DOCS_SOURCE_PATH.as_posix())
 # pylint: disable-next=import-error
 import conf as sphinx_conf
+
+def get_current_branch_name() -> str:
+    """
+    Current branch name
+    """
+    return Popen(['git', 'branch', '--show-current'], stdout=PIPE).communicate()[0].decode('utf-8').strip()
+
 
 def test_version():
     """
@@ -25,7 +32,16 @@ def test_version():
     
     assert version == __version__, f'Version in `__init__.py` is {__version__}, but in `pyproject.toml` is {version}'
     
-    assert sphinx_conf.release == __version__, f'Version in `pyproject.toml` is {version}, but in `conf.py` is {sphinx_conf.release}'
+    assert sphinx_conf.release == version, f'Version in `pyproject.toml` is {version}, but in `conf.py` is {sphinx_conf.release}'
+    
+    try:
+        branch_name = get_current_branch_name()
+        if 'release' in branch_name:
+            git_version = branch_name.split('/')[1]
+            assert git_version == version, f'Version in `pyproject.toml` is {version}, but the branch name is {branch_name}'
+    except Exception as e:
+        if isinstance(e, AssertionError):
+            raise
     
     
 if __name__ == '__main__':
